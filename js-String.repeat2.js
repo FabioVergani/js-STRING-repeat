666fcd83a6514c68e4567cd93a338d6fb10b0b8e
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-25 17:00:14 +0200

    Update js-String.repeat2.js

--- js-String.repeat2.js
+++ js-String.repeat2.js
@@ -1,10 +1,11 @@
 //#
-(Object.refill=function(o,p,e){o[p]=p in o ?o[p]:e||null}).by=function(o,e){eachKey(e,function(p){Object.refill(o,p,e[p])})};
+Object.refill=function(o,p,e){o[p]=p in o ?o[p]:e||null}
 
 //#
 (function(e,r,s,f){
 r(e,s,f.bind({p:'pop'}));
-r(e,s+'TopDown',f.bind({p:'shift'}))
+r(e,s+'TopDown',f.bind({p:'shift'}));
+r.by=function(o,e){eachKey(e,function(p){Object.refill(o,p,e[p])})};
 })(Array,Object.refill,'Consume',function(o,f){var e,p=this.p,m=[];while(e=o[p]()){f(e);m.push(e)};return m});
 
 //#
@@ -14,11 +15,6 @@ function eachKey(o,f){return Array.Consume(Object.keys(o),f)};
 Object.refill.by(String.prototype,{
  repeat:function(n){var s=this;if(s!==null && n>0){var t='';while(n>0){if(n%2===1){t+=s};s+=s;n>>=1};s=t};return s}//,...
 });
-
-//#
-
-
-
 
 
 console.log("6".repeat(3));
