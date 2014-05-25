82712b54ed176e45e2d8e1e9ca9e8bbde8ffbfd2
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-25 16:43:03 +0200

    Create js-String.repeat2.js

---
+++ js-String.repeat2.js
@@ -0,0 +1,29 @@
+//#
+(Object.refill=function(o,p,e){o[p]=p in o ?o[p]:e||null}).by=function(o,e){eachKey(e,function(p){Object.refill(o,p,e[p])})};
+
+//#
+(function(e,r,s,f){
+r(e,s,f.bind({p:'pop'}));
+r(e,s+'TopDown',f.bind({p:'shift'}))
+})(Array,Object.refill,'Consume',function(o,f){var e,p=this.p,m=[];while(e=o[p]()){f(e);m.push(e)};return m});
+
+//#
+function eachKey(o,f){return Array.Consume(Object.keys(o),f)};
+
+//#
+Object.refill.by(String.prototype,{
+ repeat:function(n){var s=this;if(s!==null && n>0){var t='';while(n>0){if(n%2===1){t+=s};s+=s;n>>=1};s=t};return s}//,...
+});
+
+//#
+
+
+
+
+
+console.log("6".repeat(3));
+
+
+
+
+
