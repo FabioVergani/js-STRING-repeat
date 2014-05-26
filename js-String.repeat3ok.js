4e06881ddaa55dec62d2b449dea2a414f5c81c6c
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-26 20:11:30 +0200

    Update js-String.repeat3ok.js

--- js-String.repeat3ok.js
+++ js-String.repeat3ok.js
@@ -1,7 +1,10 @@
-String.repeat=function(s,n){var t='';if(n>1){while(n>0){if(n%2===1){t+=s};s+=s;n>>=1}}else{t=s};return t};
+String.repeat=function(s,n){var t='';if(n>1){while(n>0){if(n%2===1){t+=s};s+=s;n>>=1}}else{t=n<=0?'':s};return t};
 
+/*
 var k=1,test=String.repeat("a",k)
 console.log(test,k+"=="+test.length,k==test.length)
-
 var k=145,test=String.repeat("a",k)
+console.log(test,k+"=="+test.length,k==test.length)
+*/
+var k=0,test=String.repeat("a",k)
 console.log(test,k+"=="+test.length,k==test.length)
