4e0792c70ba9d4225ca2f459b1e6955f737553f4
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-26 20:17:57 +0200

    Create String.padNok.js

---
+++ String.padNok.js
@@ -0,0 +1,11 @@
+String.repeat=function(s,n){var t='';if(n>1){while(n>0){if(n%2===1){t+=s};s+=s;n>>=1}}else{t=n<=0?'':s};return t};
+
+
+
+String.padN=function(s,i,t){return t?i?String.repeat(t,1+i-s.length)+s:s:s}
+
+console.log(String.padN("1",0));
+console.log(String.padN("1",0,'-'));
+console.log(String.padN("1",1,'x'));
+console.log(String.padN("1",2,'x'));
+console.log(String.padN("1",23,'x'));
